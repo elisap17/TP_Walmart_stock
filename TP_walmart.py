d5a0fde434ef2ace862e9e83806d05d3faff909f
Elisa <elisa-petit@wanadoo.fr>
2021-01-15 23:55:18 +0100

    2nd commit

--- TP_walmart.py
+++ TP_walmart.py
@@ -41,7 +41,7 @@ df2.show(5)
 #sql
 #configuration pour utiliser SQL
 df.createOrReplaceTempView("df_sql") 
-spark.sql("SELECT High/Volume as HV_Ratio FROM df_sql ").show()
+spark.sql("SELECT High/Volume as HV_Ratio FROM df_sql").show()
 
 # 6) Jour avec le prix le plus haut
 #python
@@ -55,6 +55,26 @@ spark.sql("SELECT Date FROM df_sql WHERE High = (SELECT MAX(High) from df_sql)")
 df.agg(avg("Close").alias("Moyenne_Close")).show()
 
 #sql
-spark.sql("SELECT AVG(Close) as Moyenne_Close FROM df_sql ").show()
+spark.sql("SELECT AVG(Close) as Moyenne_Close FROM df_sql").show()
 
 # 8) Maximum et minimum de la colonne Volume
+#python
+df.select(min("Volume").alias("Minimum_Volume"),max("Volume").alias("Maximum_Volume")).show()
+
+#sql
+spark.sql("SELECT MIN(Volume) as Minimum_Volume, MAX(Volume) as Maximum_Volume FROM df_sql").show()
+
+# 9) Close < 60
+#python
+print(df.filter("Close < 60").count())
+
+#sql
+spark.sql("SELECT count(*) as Nb_jours FROM df_sql WHERE Close < 60").show()
+
+# 10) Pourcentage avec High > 80
+#python
+print((df.filter("High > 80").count()/df.count())*100)
+
+#sql
+spark.sql("SELECT (SELECT COUNT(High) FROM df_sql WHERE High > 80) / COUNT(High) * 100 as Pourcentage FROM df_sql").show()
+
